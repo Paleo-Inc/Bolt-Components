d6f01114e6a4fc6594734f5b069ba012d8f42a58
Max Pauwels <91980500+maxpaleo@users.noreply.github.com>
2024-01-13 09:54:57 -0800

    Revert "Updated index file"
    This reverts commit 467b97da20086879a12861f5cd7b58f5ad19e6b4.

--- next.config.js
+++ next.config.js
@@ -2,13 +2,14 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  basePath: "/paleo-inc.github.io/Bolt-Components",
-  assetPrefix: "/paleo-inc.github.io/Bolt-Components/",
-  output: "export",
-  // Uncomment the following lines if you need them
-  // trailingSlash: true,
-  // skipTrailingSlashRedirect: true,
-  // distDir: 'dist',
-};
-
-module.exports = nextConfig;
+    basePath: "/paleo-inc.github.io/Bolt-Components",
+    assetPrefix: "/paleo-inc.github.io/Bolt-Components/",
+    output: 'export',
+    // Uncomment the following lines if you need them
+    // trailingSlash: true,
+    // skipTrailingSlashRedirect: true,
+    // distDir: 'dist',
+  }
+  
+  module.exports = nextConfig;
+  
