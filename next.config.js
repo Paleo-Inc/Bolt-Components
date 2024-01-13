80986424e25c40e43fe1712bbd055fc7712e6d72
Max Pauwels <91980500+maxpaleo@users.noreply.github.com>
2024-01-13 09:41:19 -0800

    Next config update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
-module.exports = {
-  basePath: "/paleo-inc.github.io/Bolt-Components",
-  assetPrefix: "/paleo-inc.github.io/Bolt-Components/",
-  output: 'standalone'
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    basePath: "/paleo-inc.github.io/Bolt-Components",
+    assetPrefix: "/paleo-inc.github.io/Bolt-Components/",
+    output: 'export',
+    // Uncomment the following lines if you need them
+    // trailingSlash: true,
+    // skipTrailingSlashRedirect: true,
+    // distDir: 'dist',
+  }
+  
+  module.exports = nextConfig;
+  
